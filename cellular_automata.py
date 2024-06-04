687c9998c455776a7118d70ce4bbc70b82cdd2c2
Angie Solis <angie.solismanzano@ucr.ac.cr>
2024-06-03 20:11:48 -0600

    Initial code for cellular automata
    Co-authored-by: emilia_viq <EmiliaViq@users.noreply.github.com>
    Co-authored-by: Luis Solano <GoninDS@users.noreply.github.com>

--- cellular_automata.py
+++ cellular_automata.py
@@ -1,22 +1,121 @@
 # Luis Solano, Angie Solís y Emilia Víquez
 
+import argparse
+import pygame
+import numpy
 
 # Global variables
-matrix = [800][800]
-neighborhood = 2
 
-import argparse
+size = 200
+matrix = [[0 for _ in range(size)] for _ in range(size)]
+new_vector = [size]
+neighborhood = 1
+file_path = "./data.txt"
 
-def main(arg1, arg2, arg3):
-    print(f"Argument 1: {arg1}")
-    print(f"Argument 2: {arg2}")
-    print(f"Argument 3: {arg3}")
+def determine_value(value1, value2, value3, rule):
+    new_value = 0
+    if (value1 == 1):
+        if (value2 == 1):
+            if (value3 == 1):
+                new_value = rule[7] # 111
+            else: # value3 == 0
+                new_value = rule[6] # 110
+        else: # value2 == 0
+            if (value3 == 1):
+                new_value = rule[5] # 101
+            else: # value3 == 0
+                new_value = rule[4] # 100
+    else:
+        if (value2 == 1):
+            if (value3 == 1):
+                new_value = rule[3] # 011
+            else: # value3 == 0
+                new_value = rule[2] # 010
+        else: # value2 == 0
+            if (value3 == 1):
+                new_value = rule[1] # 001
+            else: # value3 == 0
+                new_value = rule[0] # 000
+    return new_value
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Input three arguments")
-    parser.add_argument("arg1", type=str, help="Tneighborhoodirst argument")
-    parser.add_argument("arg2", type=str, help="The second argument")
-    parser.add_argument("arg3", type=str, help="The third argument")
+def algorithm(neighborhood, row, rule):
+    global matrix
+    global new_vector
+    for i in range(0, size):
+        # value2 value1 value3
+        value1 = matrix[row][i]
+        if (i - neighborhood < 0):
+            value2 = matrix[row][size-1]  # last value
+            value3 = matrix[row][i+neighborhood]
+        elif (i + neighborhood >= size):
+            value2 = matrix[row][i-neighborhood]
+            value3 = matrix[row][0]  # first value
+        else: # no special case
+            value2 = matrix[row][i-neighborhood]
+            value3 = matrix[row][i+neighborhood]
+        new_value = determine_value(value2, value1, value3, rule)
 
-    args = parser.parse_args()
-    main(args.arg1, args.arg2, args.arg3)
+        print("new_value = ", new_value, " values: ", value2, ",", value1, ",", value3)
+
+        new_vector[i] = new_value
+        # color_pixel(new_value, row, i)
+        
+def color_pixel(new_value, row, column):
+    color = (255, 255, 255)
+    if (new_value == 1):
+        color = (1, 1, 1)
+    pygame.draw.rect(screen, color, pygame.Rect(row + 5, column + 5, 5, 5))
+    pygame.display.update()
+
+def main():
+    global matrix
+    global new_vector
+    global screen
+
+    with open(file_path, 'r') as file:
+        data = file.read()
+        counter = len(data) -1
+        for i in range(0,counter):
+            matrix[0][i] = int(data[i])
+            
+        print(matrix[0])
+    
+    
+
+    # Regla (de 0 a 255): pasar a binario
+    rule_number = int(input("Insert rule: "))
+
+    # Convert the integer to a binary string and remove the '0b' prefix
+    binary_representation = bin(rule_number)[2:]
+
+    # Ensure the binary string has exactly 8 digits by padding with leading zeros if necessary
+    binary_representation = binary_representation.zfill(8)
+
+    # Convert the binary string to an array of digits
+    rule = [int(digit) for digit in binary_representation]
+    print("Rule ", rule)
+
+
+    # Screen configuration
+    screen = pygame.display.set_mode((1000, 1000))
+    pygame.display.set_caption("Cellular automata")
+    screen.fill((255, 255, 255))
+
+    can_continue = True
+
+    # while(can_continue):
+    for i in range(0, 1):
+        for event in pygame.event.get():
+        # Event to quit found, then quit
+            if event.type == pygame.QUIT:
+                can_continue = False
+                break
+
+        algorithm(neighborhood, i, rule)
+        matrix[i+1] = new_vector  # TODO: reescribir
+        print(matrix)
+    
+    
+
+
+main()
